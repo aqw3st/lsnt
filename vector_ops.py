81dbd4dd71e2c1b6e7bee005ecb66a6fb6d5d5f5
aqw3st <aqw3st@icloud.com>
2022-07-18 14:54:26 +0300

    add shape and angle in degree

--- vector_ops.py
+++ vector_ops.py
@@ -2,9 +2,10 @@
 import math
 
 class Vector():
+
   def __init__(self, vector):
     self.vector = vector
-  
+   
   def __repr__(self):
     return f"vector({self.vector})"
 
@@ -15,10 +16,16 @@ class Vector():
     return Vector(res_vec)
 
   def __mul__(self, vec2):
+    assert Vector(self.vector).shape() == Vector(vec2).shape(), 'Vectors dimensions don`t match'
     res_vec = []
     for i, j in zip(self.vector, vec2.vector):
       res_vec.append(i*j)
-    return sum(res_vec)
+    return res_vec
+
+  def shape(self):
+    if len(self.vector) == 0: return (0, )
+    if isinstance(self.vector[0], int): return (len(self.vector), )
+    else: return len(self.vector), len(self.vector[0])
 
   def mag(self):
     return math.sqrt(sum([el**2 for el in self.vector]))
@@ -34,20 +41,25 @@ class Vector():
       self.vector[i] *= k
     return Vector(self.vector)
 
-  
-  # @staticmethod
-  def angle(vec1, vec2):
-    cos_degree = vec1 * vec2 / (vec1.mag() * vec2.mag())
-    
-    return cos_degree
+  @staticmethod
+  def angle(vec1, vec2, degree = False):
+    rad = math.acos(vec1 * vec2 / (vec1.mag() * vec2.mag()))
+    if degree == True:
+      return rad * (180/math.pi)
+    return rad
 
+  @staticmethod
+  def cross_product(vec1, vec2, degree = 0):
+    assert vec1.shape() == (3,), f"""Only (3,) vectors allowed, but {vec1} is {vec1.shape()}"""
+    assert vec2.shape() == (3,), f"""Only (3,) vectors allowed, but {vec2} is {vec2.shape()}"""
+    raise ValueError("Not implemented yet") 
 
-
-vec1 = Vector([1,2,3])
-vec2 = Vector([4,5,6])
-# print(vec1+vec2)
-# print(vec1*vec2)
+vec1 = Vector([1,2,3,10])
+vec2 = Vector([4,5,6,8])
+# print(vec1.shape())
+# print(Vector.cross_product(vec1, vec2))
+print(vec1+vec2)
+print(vec1*vec2)
 # print(vec1.mag())
 # print(vec1.dot(vec2))
-# print(angle(vec1, vec2))
-print(Vector.angle(vec1, vec2))
+# print(Vector.angle(vec1, vec2, degree = True))
